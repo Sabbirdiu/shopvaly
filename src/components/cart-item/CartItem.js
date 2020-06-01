c5e23202dbdcbb84f74dc32e00209f24ba0481bd
Sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-06-01 10:20:26 +0600

    update engines

--- src/components/cart-item/CartItem.js
+++ src/components/cart-item/CartItem.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import './cart.scss'
-const CartItem = ({item: {imageUrl, price, name,quantity}}) => {
-    return (
-        <div className='cart-item' >
-            <img src={imageUrl} alt='item' />
-            <div className='item-details'>
-                <span className='name' >{name}</span>
-                <span className='price' > {quantity} x ${price}</span>
+import React from 'react';
+import './cart.scss';
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+  return (
+    <div className='cart-item'>
+      <img src={imageUrl} alt='item' />
+      <div className='item-details'>
+        <span className='name'>{name}</span>
+        <span className='price'>
+          {' '}
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-            
-        </div>
-    )
-}
-
-export default  CartItem
+export default CartItem;
